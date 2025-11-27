62b011d62e3edbbac9c17b4a4009ffa39d45294f
Aleksandar Hubanov <ahubanov@student.ethz.ch>
2025-11-27 21:35:32 +0200

    setup attempt

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/".insteadOf "git@github.com:"
+git submodule update --init --recursive
+export DYNAMIC_REPO_PATH=${DYNAMIC_REPO_PATH}
+git clone https://x-access-token:${GITHUB_TOKEN}@github.com/${DYNAMIC_REPO_PATH}.git data/project-repository
+
+if [ ! -d "data/project-repository" ]; then
+    echo "Error: Failed to clone repository into data/project-repository."
+    exit 1
+fi
+
+cd data/project-repository
+git checkout ${PARENT_COMMIT}
+
+npm install --ignore-scripts 
